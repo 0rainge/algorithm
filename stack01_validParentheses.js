cc319da91d0354601814a0d09d9a51d2db464372
0rainge <Orange96@aliyun.com>
2019-03-22 16:28:31 +0800

    stack01

--- stack01_validParentheses.js
+++ stack01_validParentheses.js
@@ -2,6 +2,15 @@
  * @param {string} s
  * @return {boolean}
  */
+
+// # 思路：
+
+// 1. 设置栈：左边入栈，右边遇到匹配的出栈，遇到不匹配的就凉了，遍历结束判断栈是否为空
+
+// 2. 通过obj的key-value值进行左右匹配
+
+// 3. 栈的实现：string：入栈+出栈subString(0,length-1)或array:入栈push，出栈pop
+
 var isValid = function (s) {
     // s.split("");
     var str = "";
@@ -11,7 +20,7 @@ var isValid = function (s) {
         '{': '}'
     }
     for (let i = 0; i < s.length; i++) {
-        console.log(str);
+        // console.log(str);
         if (map[s[i]]) {
             str += s[i];
         } else if (str && map[str[str.length - 1]] == s[i]) {
@@ -22,9 +31,7 @@ var isValid = function (s) {
             return false;
         }
     }
-    
-    // console.log(333);
-    // console.log(str);
+
     return (str.length == 0);
 };
 
